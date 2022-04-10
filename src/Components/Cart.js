c663af537219a09109283ebd77b3f716fe48344a
mkumar0802 <mkumar0802@gmail.com>
2022-04-10 13:36:41 +0530

    bug fix

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -25,7 +25,7 @@ function Cart() {
 
     }, [])
 
-    const setData = (id, price,photo,name) => {
+    const setData = (id, price, photo, name) => {
         localStorage.setItem('id', id)
         localStorage.setItem('name', name)
         localStorage.setItem('photo', photo)
@@ -41,18 +41,17 @@ function Cart() {
                 <h1 className="sm:text-xl md:text-3xl ">Cart details</h1>
 
                 <div className="grid grid-cols-2 gap-5  md:grid-cols-3 p-2">
-                   
                     <img
                         className=" object-cover rounded-xl md:w-full  md:h-4/6  h-5/6 w-full   "
-                        alt="cart"
+                        alt="Briyani"
                         src={photo}
                     ></img>
 
                     <div class="mb-6 text-white">
                         <p className="text-base md:text-xl  ">{name}</p>
-                        <p className="text-base md:text-lg   ">₹{price}</p>
+                        <p className="text-base md:text-lg   ">₹{price} {photo}</p>
                         <p className="text-base md:text-lg   ">Total Price</p>
-                        <p className="text-base md:text-lg   ">₹{price*count}</p>
+                        <p className="text-base md:text-lg   ">₹{price * count}</p>
                     </div>
 
                     <div className="justify-center ml-6 ">
@@ -63,7 +62,7 @@ function Cart() {
                             <button onClick={() => setCount(count + 1)} className={`  ${count >= 100 ? 'bg-red-600 opacity-50 cursor-not-allowed px-4 py-1 rounded-md ' : 'rounded-md px-4 py-1 my  bg-red-600 hover:bg-red-700'} `}>+</button>
                         </div>
                         <div className="justify-center py-5 px text-white font-bold">
-                            <button onClick={() => setData(id,price )}   className={`  ${count === 0 ? '  bg-red-600 opacity-50 cursor-not-allowed px-12 py-8 rounded-md ' : 'rounded-md px-10 py-4 my  bg-red-600 hover:bg-red-700'} `}><Link to="/paymentconfirmation">Next </Link><Outlet /> </button>
+                            <button onClick={() => setData(id, price)} className={`  ${count === 0 ? '  bg-red-600 opacity-50 cursor-not-allowed px-12 py-8 rounded-md ' : 'rounded-md px-10 py-4 my  bg-red-600 hover:bg-red-700'} `}><Link to="/paymentconfirmation">Next </Link><Outlet /> </button>
                         </div>
                     </div>
                 </div>
