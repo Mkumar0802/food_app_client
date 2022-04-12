e99d1b1ae84b388d2a8a8c07dc9daf5f899cd3e8
mkumar0802 <mkumar0802@gmail.com>
2022-04-12 10:51:36 +0530

    aligment fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,57 +4,58 @@ import Downbar from './Downbar';
 
 
 const Navbar = () => {
- 
-    let [open,setOpen]=useState(false);
+
+  let [open, setOpen] = useState(false);
   return (
     <div className='shadow-md w-full relative top-0 left-0  '>
       <div className='md:flex items-center justify-between bg-slate-900   py-4 md:px-10 px-7'>
-      <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+        <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
       '>
-        <span className='text-3xl mr-1 pt-2 text-white'>
-        <Link to="/home" >CFC</Link>
-        </span >
-         
+          <span className='text-3xl mr-1 pt-2 text-white'>
+            <Link to="/home" >CFC</Link>
+          </span >
+
+        </div>
+
+        <div onClick={() => setOpen(!open)} className='text-3xl text-white absolute right-8 top-6 cursor-pointer md:hidden'>
+          <ion-icon name={open ? 'close' : 'menu'}></ion-icon>
+        </div>
+
+        <ul className={` text-white md:flex gap-3 md:items-center md:pb-0 pb-12 absolute md:static  md:z-auto z-[-1] left-0 w-3/4 md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ? '  left-20 ' : 'top-[-490px]  '}`}>
+
+
+          <li className=" rounded-lg px-4 py-2  ">
+            <Link to="/">Our Menu</Link>
+          </li>
+          <li className=" rounded-lg px-4 py-2  ">
+            <Link to="/">RESTAURANTS NEAR ME</Link>
+          </li>
+
+          <li className=" rounded-lg px-4 py-2   ">
+            <Link to="/">GIFT CARD</Link>
+          </li>
+          <li className=" rounded-lg px-4 py-2   ">
+            <Link to="/">CORPORATE ENQUIRY</Link>
+          </li>
+          <li className=" rounded-lg px-4 py-2   ">
+            <Link to="/">CONTACT</Link>
+          </li>
+          <li className="sm:hidden  md:block  rounded-lg px-4 py-2  bg-red-600 hover:bg-red-700 ">
+            <Link to="/">DOWNLOAD APP</Link>
+          </li>
+          <li className=" rounded-lg px-4 py-2  bg-red-600 hover:bg-red-700  ">
+            <Link to="/">ORDER ONLINE NOW</Link>
+
+          </li>
+
+
+        </ul>
+
+        <Outlet />
       </div>
-      
-      <div onClick={()=>setOpen(!open)} className='text-3xl text-white absolute right-8 top-6 cursor-pointer md:hidden'>
-      <ion-icon name={open ? 'close':'menu'}></ion-icon>
-      </div>
 
-      <ul className={` text-white md:flex gap-3 md:items-center md:pb-0 pb-12 absolute md:static  md:z-auto z-[-1] left-0 w-3/4 md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${open ?  '  top-20 ':'top-[-490px]  '}`}>
-        
-      
-            <li className=" rounded-lg px-4 py-2  ">
-                <Link to="/">Our Menu</Link>
-            </li>
-            <li className=" rounded-lg px-4 py-2  ">
-                <Link to="/">RESTAURANTS NEAR ME</Link>
-            </li>
-
-            <li className=" rounded-lg px-4 py-2   ">
-                <Link to="/">GIFT CARD</Link>
-            </li>
-            <li className=" rounded-lg px-4 py-2   ">
-                <Link to="/">CORPORATE ENQUIRY</Link>
-            </li>
-            <li className=" rounded-lg px-4 py-2   ">
-                <Link to="/">CONTACT</Link>
-            </li>
-            <li className="sm:hidden  md:block  rounded-lg px-4 py-2  bg-red-600 hover:bg-red-700 ">
-                <Link to="/">DOWNLOAD APP</Link>
-            </li>
-            <li className=" rounded-lg px-4 py-2  bg-red-600 hover:bg-red-700  ">
-                <Link to="/">ORDER ONLINE NOW</Link>
-                
-            </li>
-
-
-      </ul>
-      
-      <Outlet/>
-      </div>
-      <Downbar/>
     </div>
+
   )
 }
 
--- src/Components/Payment.js
+++ src/Components/Payment.js
@@ -16,34 +16,34 @@ export function Payment() {
     // }, [])
 
     return (
-        <div className="text-white ">
+        <div className=" ">
 
             <div className="flex justify-center p-10  ">
-                <h1 className="text-xl font-semibold  ">Payment Details</h1>
+                <h1 className="text-xl font-semibold text-white ">Payment Details</h1>
             </div>
             <div className="md:flex md:justify-center p-5  grid grid-rows-2 justify-center ">
                 <form class="w-full max-w-lg">
                     <div class="flex flex-wrap mx-3 mb-6">
                         <div class="w-3/4 md:w-2/4  px-3">
-                            <label class="block uppercase tracking-wide  text-xs font-bold mb-2" for="grid-name">
+                            <label class="block uppercase tracking-wide  text-xs font-bold mb-2 text-white" for="grid-name text-white">
                                 Name
                             </label>
                             <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="text" placeholder="Cardholder Name" />
-                            <p class=" text-xs italic">fill out all details</p>
+                            <p class=" text-xs italic text-white">fill out all details</p>
                         </div>
 
                     </div>
                     <div class="flex flex-wrap mx-3 mb-6">
                         <div class="w-3/4 md:w-2/4   px-3">
-                            <label class="block uppercase tracking-wide  text-xs font-bold mb-2" for="grid-password">
+                            <label class="block uppercase tracking-wide  text-xs font-bold mb-2 text-white" for="grid-password">
                                 Card Number
                             </label>
                             <input class="appearance-none block w-full bg-gray-200  border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="number" placeholder="XXXXXXXXXXXXXXX" />
-                            <p class=" text-xs italic">Enter 16 digit Card number</p>
+                            <p class=" text-xs italic text-white">Enter 16 digit Card number</p>
                         </div>
 
                         <div class="w-1/4 md:w-1/4 px-3 mb-6 md:mb-0">
-                            <label class="block uppercase tracking-wide  text-xs font-bold mb-2" for="grid-zip">
+                            <label class="block uppercase tracking-wide  text-xs font-bold mb-2 text-white" for="grid-zip">
                                 CVV
                             </label>
                             <input class="appearance-none block w-full bg-gray-200  border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="password" placeholder="CVV" />
@@ -51,7 +51,7 @@ export function Payment() {
                     </div>
                     <div class="flex  mx-3 mb-2 ">
                         <div class="w-2/4 md:w-1/4 px-3 mb-6 md:mb-0">
-                            <label class="block uppercase tracking-wide  text-xs font-bold mb-2" for="grid-city">
+                            <label class="block uppercase tracking-wide  text-xs font-bold mb-2 text-white" for="grid-city">
                                 Expire date
                             </label>
                             <input class="appearance-none block w-full bg-gray-200  border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-city" type="password" placeholder="MM/YY" />
@@ -60,8 +60,8 @@ export function Payment() {
 
 
                     </div>
-                    <div className="flex justify-center  py-5 px text-white font-bold">
-                    <button className=" rounded-md px-8 py-4 my  bg-red-600 hover:bg-red-700  "><Link to="/paymentconfirmation">PAY  </Link><Outlet /> </button>
+                    <div className="flex justify-center  py-5 px  font-bold">
+                    <button className=" rounded-md px-8 py-4 my  bg-red-600 hover:bg-red-700 text-white "><Link to="/paymentconfirmation">PAY  </Link><Outlet /> </button>
                 </div>
                 </form>
                
